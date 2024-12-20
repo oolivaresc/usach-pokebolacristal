6d4f9714fb9efffc51eef2b7e0cae5a692daf600
Oscar Olivares <o.olivares.c7@gmail.com>
2024-12-20 03:11:54 -0300

    Se agregan cambios de estilo

--- js/presentation/render-pokemon.js
+++ js/presentation/render-pokemon.js
@@ -1,11 +1,18 @@
 export const renderPokemon = async(pokemon) => {
     const divPokemons = document.getElementById("pokemons");
 
-    //divPokemons.querySelectorAll(".card").forEach((element)=>{
-    //  divPokemons.removeChild(element);
-    //})
-    divPokemons.innerHTML = `
+    const imgPokemon = document.createElement("img");
+    imgPokemon.setAttribute("src", pokemon.image);
+    imgPokemon.setAttribute("alt", pokemon.name);
+
+    const pPokemon = document.createElement("p");
+    pPokemon.innerHTML = pokemon.name;
+
+    divPokemons.append(imgPokemon);
+    divPokemons.append(pPokemon);
+
+    /*= `
         <img src="${pokemon.image}" alt="${pokemon.name}">
         <p>${pokemon.name}</p>
-    `;
+    `;*/
   }
